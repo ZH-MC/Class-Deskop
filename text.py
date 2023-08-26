aa30c8fe183252e355c2f1051b8cafa12cd7f21b
ZH-MC <143183290+ZH-MC@users.noreply.github.com>
2023-08-26 15:43:59 +0800

    Update text.py
    添加必要注释

--- text.py
+++ text.py
@@ -1,4 +1,5 @@
-t = 'C:/Users/Administrator/Desktop/names.txt'
+#将名字转换为js数组
+t = 'C:/Users/Administrator/Desktop/names.txt'#文件地址
 f=open(encoding='utf-8')
 line = f.readline().strip() #读取第一行
 txt=[]
@@ -7,4 +8,4 @@ while line:  # 直到读取完文件
    line = f.readline().strip()  # 读取一行文件，包括换行符
    txt.append(line)
 f.close()  # 关闭文件
-print(txt)
+print(txt)#输出js数组
